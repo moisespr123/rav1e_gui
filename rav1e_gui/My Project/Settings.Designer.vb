bf9f98f4345ace149534f06361581235df94c188
moisesmcardona <moises@moises-studios.com>
2019-04-22 15:20:02 -0400

    Changed default speed setting from 3 to 5, to match rav1e --speed argument.

--- rav1e_gui/My Project/Settings.Designer.vb
+++ rav1e_gui/My Project/Settings.Designer.vb
@@ -68,7 +68,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("3")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("5")>  _
         Public Property speed() As Integer
             Get
                 Return CType(Me("speed"),Integer)
